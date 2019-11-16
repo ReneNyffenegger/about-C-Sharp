00e5b232bf68a0a846ea34b8f61624424687ea45
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-11-16 09:02:22 +0100

    f1 -> calc

--- language/keywords/exception-handling/catch.cs
+++ language/keywords/exception-handling/catch.cs
@@ -2,16 +2,16 @@ using System;
 
 class Prg {
 
-  static int f1(int i) {
+  static int calc(int i) {
      return 42/i;
   }
 
   static void Main() {
 
      try {
-        Console.WriteLine(f1( 7));
-        Console.WriteLine(f1( 0));
-        Console.WriteLine(f1(-2));
+        Console.WriteLine(calc( 7));
+        Console.WriteLine(calc( 0));
+        Console.WriteLine(calc(-2));
      }
   //
   // Note the order of the catch blocks:

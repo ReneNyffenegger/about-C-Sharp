725753ad403ce1fa6dfdbd84508d3c217290bf9d
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-12-15 11:37:11 +0100

    remove whitespace

--- language/types/reference/strings/verbatime-interpolated-etc.cs
+++ language/types/reference/strings/verbatime-interpolated-etc.cs
@@ -6,20 +6,21 @@ class C {
 
       string txt = "Hello World";
 
-      Console.WriteLine( " He said: \"Hello World\". ");      
+      Console.WriteLine( " He said: \"Hello World\". ");
 
    //
    // Verbatim Strings (@"…")
    //
-   //    Note: the @ sign can also be used to introduce a verbatim identifer (for (int @for in … ))
+   //    Note: the @ sign can also be used to introduce a
+   //    verbatim identifer (for (int @for in … ))
    //
 
-      Console.WriteLine(@" He said: ""Hello World"". ");      
+      Console.WriteLine(@" He said: ""Hello World"". ");
 
       Console.WriteLine( " new\n line.");
       Console.WriteLine(@" new
  line.");
-   // Console.WriteLine(@" He said \"Hello World\". ");      
+   // Console.WriteLine(@" He said \"Hello World\". ");
 
 
       Console.WriteLine( " > P:\\ath\\to\\directory");
@@ -35,11 +36,9 @@ class C {
       Console.WriteLine($" interpolated> He said: {txt}");
 
    //
-   // Verbatime and interpolated
-   //
+   // Verbatim and interpolated
    //
       Console.WriteLine(@$"@$");
 
    }
-
 }
